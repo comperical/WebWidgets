6625d18b89c8b81acf809d1bed18405684106378
Daniel Burfoot <daniel.burfoot@gmail.com>
2023-10-05 14:14:58 -0400

    Updates

--- server/script/config_demo_server.py
+++ server/script/config_demo_server.py
@@ -123,11 +123,25 @@ def get_resin_config_xml():
 
     <!-- the default host, matching any host name -->
     <host id="" root-directory=".">
-      <!--
-         - webapps can be overridden/extended in the resin.xml
-        -->
-      <web-app id="/" root-directory="webapps/ROOT"/>
+        
+        <web-app id="/" root-directory="/opt/userdata/lifecode/wwiosite">
+          <welcome-file-list>index.jsp</welcome-file-list>
+        </web-app>
 
+        <!-- new, all-user generic webapp -->
+        <web-app id="/u" root-directory="/opt/userdata/widgetserve">
+            <!-- this is the main callback servlet. At some point we will probably have to change this path -->
+            <servlet-mapping url-pattern="/callback" servlet-class="io.webwidgets.core.CallBack2Me" />
+            
+            <!-- this is the main entry point to the extension operations. All non-core Widget operations lead from here -->
+            <servlet-mapping url-pattern="/extend" servlet-class="io.webwidgets.extend.ExtensionServe" />
+            
+            <!-- Blob storage servlet, see private doccode about Blob Data -->
+            <servlet-mapping url-pattern="/blobstore" servlet-class="io.webwidgets.core.BlobDataManager$BlobStorageServlet" />
+            <servlet-mapping url-pattern="/push2me" servlet-class="io.webwidgets.core.ActionJackson$Push2Me" />
+            <servlet-mapping url-pattern="/pull2you" servlet-class="io.webwidgets.core.ActionJackson$Pull2You" />   
+            <servlet-mapping url-pattern="/convert2excel" servlet-class="io.webwidgets.core.ActionJackson$ConvertGrabExcel" />  
+        </web-app>
     </host>
       
     <resin:if test="${resin_doc}">
